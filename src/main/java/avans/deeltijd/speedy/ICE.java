eb2b3c424aef05d24ae0edc35aec4dc1ffb043dc
Auk-A <auke.aardoom@gmail.com>
2022-09-22 18:36:00 +0200

    Add BEV subclass and remove carType from constructor

--- src/main/java/avans/deeltijd/speedy/ICE.java
+++ src/main/java/avans/deeltijd/speedy/ICE.java
@@ -1,24 +1,26 @@
 package avans.deeltijd.speedy;
 
+import lombok.Getter;
+
 import javax.persistence.Entity;
 import java.time.LocalDate;
 
 @Entity
 public class ICE extends Car{
-    private String carType;
+    @Getter
+    private final String carType = "Internal combustion engine";
+    @Getter
     private Engine engineType;
 
     public ICE() {
     }
 
     public ICE(Engine engineType) {
-        this.carType = "Internal combustion engine";
         this.engineType = engineType;
     }
 
     public ICE(String licensePlate, String brand, String model, String color, double value, LocalDate dateOfBuild, int paxCapacity, Engine engineType) {
         super(licensePlate, brand, model, color, value, dateOfBuild, paxCapacity);
-        this.carType = "Internal combustion engine";
         this.engineType = engineType;
     }
 }
