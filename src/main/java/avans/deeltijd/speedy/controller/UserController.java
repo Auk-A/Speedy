aefb50963a661be9817b0ade56a8b19a61f3f1b1
Auk-A <auke.aardoom@gmail.com>
2022-10-27 13:34:04 +0200

    Move business logic of user from controller to service

--- src/main/java/avans/deeltijd/speedy/controller/UserController.java
+++ src/main/java/avans/deeltijd/speedy/controller/UserController.java
@@ -2,6 +2,7 @@ package avans.deeltijd.speedy.controller;
 
 import avans.deeltijd.speedy.domain.User;
 import avans.deeltijd.speedy.repository.UserRepository;
+import avans.deeltijd.speedy.service.UserService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,10 +16,10 @@ import java.time.format.DateTimeFormatter;
 @RestController
 @RequestMapping("/user")
 public class UserController {
-    private final UserRepository userRepository;
+    private final UserService userService;
 
-    public UserController(UserRepository userRepository) {
-        this.userRepository = userRepository;
+    public UserController(UserService userService) {
+        this.userService = userService;
     }
 
     // Add a new user
@@ -29,21 +30,7 @@ public class UserController {
             @RequestParam String user_email,
             @RequestParam String date_of_birth
     ) {
-        LocalDate dateOfBirth;
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
-        try {
-            dateOfBirth = LocalDate.parse(date_of_birth, formatter);
-        } catch (Exception ignore) {
-            return new ResponseEntity<>("Date format incorrect: Should be dd-MM-yyyy", HttpStatus.CONFLICT);
-        }
-        try {
-            User createdUser = new User(first_name, last_name, user_email, dateOfBirth);
-            userRepository.save(createdUser);
-            return new ResponseEntity<>("New user created", HttpStatus.CREATED);
-        } catch (Exception ignore) {
-            return new ResponseEntity<>("User already exists", HttpStatus.CONFLICT);
-        }
-
+        return userService.newUser(first_name, last_name, user_email, date_of_birth);
     }
 
 }
--- src/main/java/avans/deeltijd/speedy/service/UserService.java
+++ src/main/java/avans/deeltijd/speedy/service/UserService.java
@@ -1,8 +1,14 @@
 package avans.deeltijd.speedy.service;
 
+import avans.deeltijd.speedy.domain.User;
 import avans.deeltijd.speedy.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
 
 @Service
 public class UserService {
@@ -11,5 +17,22 @@ public class UserService {
     @Autowired
     public UserService(UserRepository userRepository) {
         this.userRepository = userRepository;
+    }
+
+    public ResponseEntity newUser(String first_name, String last_name, String user_email, String date_of_birth) {
+        LocalDate dateOfBirth;
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+        try {
+            dateOfBirth = LocalDate.parse(date_of_birth, formatter);
+        } catch (Exception ignore) {
+            return new ResponseEntity<>("Date format incorrect: Should be dd-MM-yyyy", HttpStatus.CONFLICT);
+        }
+        try {
+            User createdUser = new User(first_name, last_name, user_email, dateOfBirth);
+            userRepository.save(createdUser);
+            return new ResponseEntity<>("New user created", HttpStatus.CREATED);
+        } catch (Exception ignore) {
+            return new ResponseEntity<>("User already exists", HttpStatus.CONFLICT);
+        }
     }
 }
